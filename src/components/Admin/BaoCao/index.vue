510e8077d6852c468e1286df3e249e6512637b2c
thanhchin01 <thanhchinn0523@gmail.com>
2025-04-04 16:51:09 +0700

    admin baocao dsccdacap thongke

---
+++ src/components/Admin/BaoCao/index.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="card">
+        <div class="card-header bg-secondary">
+            <h2 class=" text-light"><i class="fa-solid fa-layer-group"></i> Báo Cáo</h2>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card bg-primary">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-user text-dark" style="font-size: 40px;"></i>
+                        <h4 class="card-title mt-2  text-dark">Tài Khoản</h4>
+                        <h6 class="card-subtitle mb-2 text-body-secondary text-dark">100</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card bg-warning">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-book-open-reader text-dark" style="font-size: 40px;"></i>
+                        <h4 class="card-title mt-2  text-dark">Chứng Chỉ</h4>
+                        <h6 class="card-subtitle mb-2 text-body-secondary text-dark">100</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card bg-success">
+                    <div class="card-body text-center">
+                        <i class="fa-regular fa-file text-dark" style="font-size: 40px;"></i>
+                        <h4 class="card-title mt-2  text-dark">Báo Cáo</h4>
+                        <h6 class="card-subtitle mb-2 text-body-secondary text-dark">9</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            <h4>Danh Sách Báo Cáo</h4>
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#them">Thêm Báo Cáo</button>
+        </div>
+
+        <!-- Modal thêm báo cáo -->
+        <div class="modal fade" id="them" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Thêm Báo Cáo</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label>Tiêu Đề</label>
+                                <input type="text" class="form-control mb-2"  id="">
+                            </div>
+                            <div class="col-lg-6">
+                                <label>Ngày Tạo</label>
+                                <input type="date" class="form-control mb-2" id="">
+                            </div>
+                        </div>
+                        <label>Nội Dung</label>
+                        <input type="text" class="form-control mb-2" style="height: 70px; font-size: 20px;" id="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                        <button type="button" class="btn btn-primary">Tạo Báo Cáo</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <table class="table table-hover table-bordered mt-3">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Tiêu Đề</th>
+                    <th>Người Tạo</th>
+                    <th>Ngày Tạo</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="align-middle">1</th>
+                    <td class="align-middle">Báo cáo tháng 4</td>
+                    <td class="align-middle">Admin</td>
+                    <td class="align-middle">1/1/2025</td>
+                    <td class="align-middle text-center">
+                        <button class="btn btn-primary">Xem Chi Tiết</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
---
+++ src/components/Admin/ThongKe/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <h2><i class="fa-solid fa-chart-simple"></i> Thống Kê</h2>
+    <div class="row mt-3">
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                        <h3 class="card-title mt-2">Số Lượng Giao Dịch</h3>
+                        <h6 class="card-subtitle mb-2 text-body-secondary mt-3">2</h6>
+                    </div>
+                    <i class="fa-solid fa-file mt-3 text-dark" style="font-size: 50px;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                        <h3 class="card-title mt-2">Tổng Doanh Thu</h3>
+                        <h6 class="card-subtitle mb-2 text-body-secondary mt-3">1.000.000 VND</h6>
+                    </div>
+                    <i class="fa-solid fa-dollar-sign mt-3 text-dark" style="font-size: 50px;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <table class="table table-hover table-bordered">
+        <thead class="bg-secondary text-white">
+            <tr>
+                <th>STT</th>
+                <th>Ngày</th>
+                <th>Số Lượng Giao Dịch</th>
+                <th>Doanh Thu</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>1/1/2025</th>
+                <th>2</th>
+                <th>1.000.000 VND</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
