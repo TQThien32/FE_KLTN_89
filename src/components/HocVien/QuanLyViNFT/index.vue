e14e049fb2384d8d003b680a70d03bbfe8d9139f
TuanTran-mem <phuoctuan2308@gmail.com>
2025-04-09 13:37:16 +0700

    yeucau va ql vi

--- src/components/HocVien/QuanLyViNFT/index.vue
+++ src/components/HocVien/QuanLyViNFT/index.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        Quản lý ví NFT
+    <div class="card">
+        <div class="card-body d-flex justify-content-between">
+            <h3>Ví NFT Chứng Chỉ</h3>
+            <div class="d-flex justify-content-center">
+                <h5 class="mt-1">địa chỉ ví</h5>
+                <i class="fa-solid fa-wallet ms-4 fa-2x"></i>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="anh chung chi">
+                    <div class="card-body">
+                        <p class="card-text">ID Token</p>
+                        <p class="card-text">MetaData URL</p>
+                        <div class="d-flex justify-content-between">
+                            <button class="btn btn-outline-primary">Kiểm tra dữ liệu</button>
+                            <button class="btn btn-outline-danger">Chia sẻ NFT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style>
-    
-</style>
+<style></style>
--- src/components/HocVien/YeuCau/index.vue
+++ src/components/HocVien/YeuCau/index.vue
@@ -1,13 +1,87 @@
 <template>
-    <div>
-        đây là trang yêu cầu
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Gửi Yêu Cầu Cấp Chứng Chỉ</h4>
+                    </div>
+                    <div class="card-body">
+                        <label class="mt-2" for="">Tên Học Viên</label>
+                        <input class="mt-2 form-control" type="text">
+                        <label class="mt-2" for="">Số CCCD</label>
+                        <input class="mt-2 form-control" type="text">
+                        <label class="mt-2" for="">Số Điện Thoại</label>
+                        <input class="mt-2 form-control" type="number">
+                        <label class="mt-2 w-100" for="">Gửi đến</label>
+                        <select class="mt-2 w-100" name="" id="" style="height: 38px;">
+                            <option value="">Tất cả</option>
+                            <option value="">Trường ....</option>
+                            <option value="">Tổ chức Toiec</option>
+                        </select>
+                        <label class="mt-2" for="">Nội Dung</label>
+                        <textarea class="mt-2 form-control" name="" id=""></textarea>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#XNgui">Gửi</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Danh Sách Yêu Cầu Cấp Đã Gửi</h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr class="align-middle text-center">
+                                    <th>#</th>
+                                    <th>Gửi đến</th>
+                                    <th>Nội Dung</th>
+                                    <th>Thời Gian Gửi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th class="align-middle text-center">1</th>
+                                    <td>Trường ...</td>
+                                    <td>gvbhjn</td>
+                                    <td>ngày giờ gửi</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal xác nhận gửi-->
+    <div class="modal fade" id="XNgui" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Có Gửi Yêu Cầu</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-warning" role="alert">
+                        <p>Sẽ gửi yêu cầu cấp chứng chỉ đến Trường ....</p>
+                        <p>Với Nội dung: ....</p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Gửi</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style>
-    
-</style>
+<style></style>
