a6536921a44ca65aa3659c9f199ac835be388ba3
Tram Ngoc Quoc <tramquoc476@gmail.com>
2025-04-11 22:51:38 +0700

    sua to chuc

--- src/components/HocVien/YeuCau/index.vue
+++ src/components/HocVien/YeuCau/index.vue
@@ -1,57 +1,55 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-12">
-                <div class="card">
-                    <div class="card-header bg-primary">
-                        <h4 class="text-light">Gửi Yêu Cầu Cấp Chứng Chỉ</h4>
-                    </div>
-                    <div class="card-body">
-                        <label class="mt-2" for="">Tên Học Viên</label>
-                        <input class="mt-2 form-control" type="text">
-                        <label class="mt-2" for="">Số CCCD</label>
-                        <input class="mt-2 form-control" type="text">
-                        <label class="mt-2" for="">Số Điện Thoại</label>
-                        <input class="mt-2 form-control" type="number">
-                        <label class="mt-2 w-100" for="">Gửi đến</label>
-                        <select class="mt-2 w-100" name="" id="" style="height: 38px;">
-                            <option value="">Tất cả</option>
-                            <option value="">Trường ....</option>
-                            <option value="">Tổ chức Toiec</option>
-                        </select>
-                        <label class="mt-2" for="">Nội Dung</label>
-                        <textarea class="mt-2 form-control" name="" id="" placeholder="Nhập nội dung yêu cầu"></textarea>
-                    </div>
-                    <div class="card-footer text-end">
-                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#XNgui">Gửi</button>
-                    </div>
+    <div class="row">
+        <div class="col-lg-4 col-md-12">
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h4 class="text-light">Gửi Yêu Cầu Cấp Chứng Chỉ</h4>
+                </div>
+                <div class="card-body">
+                    <label class="mt-2" for="">Tên Học Viên</label>
+                    <input class="mt-2 form-control" type="text">
+                    <label class="mt-2" for="">Số CCCD</label>
+                    <input class="mt-2 form-control" type="text">
+                    <label class="mt-2" for="">Email</label>
+                    <input class="mt-2 form-control" type="email">
+                    <label class="mt-2 w-100" for="">Gửi đến</label>
+                    <select class="mt-2 w-100" name="" id="" style="height: 38px;">
+                        <option value="">Tất cả</option>
+                        <option value="">Trường ....</option>
+                        <option value="">Tổ chức Toiec</option>
+                    </select>
+                    <label class="mt-2" for="">Nội Dung</label>
+                    <textarea class="mt-2 form-control" name="" id="" placeholder="Nhập nội dung yêu cầu"></textarea>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#XNgui">Gửi</button>
                 </div>
             </div>
-            <div class="col-lg-8 col-md-12">
-                <div class="card">
-                    <div class="card-header bg-secondary">
-                        <h4 class="text-light">Danh Sách Yêu Cầu Cấp Đã Gửi</h4>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr class="align-middle text-center">
-                                    <th>#</th>
-                                    <th>Gửi đến</th>
-                                    <th>Nội Dung</th>
-                                    <th>Thời Gian Gửi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th class="align-middle text-center">1</th>
-                                    <td>Trường ...</td>
-                                    <td>gvbhjn</td>
-                                    <td>ngày giờ gửi</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+        </div>
+        <div class="col-lg-8 col-md-12">
+            <div class="card">
+                <div class="card-header bg-secondary">
+                    <h4 class="text-light">Danh Sách Yêu Cầu Cấp Đã Gửi</h4>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr class="align-middle text-center">
+                                <th>#</th>
+                                <th>Gửi đến</th>
+                                <th>Nội Dung</th>
+                                <th>Thời Gian Gửi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th class="align-middle text-center">1</th>
+                                <td>Trường ...</td>
+                                <td>gvbhjn</td>
+                                <td>ngày giờ gửi</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
