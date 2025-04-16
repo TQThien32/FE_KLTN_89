99a6064025679d9741959cf16255ab7773296f43
TQThien32 <tranqthien3203@gmail.com>
2025-04-16 23:05:38 +0700

    edit bot

--- src/layout/components/Bot.vue
+++ src/layout/components/Bot.vue
@@ -3,73 +3,71 @@
         <p class="mb-0">Copyright © 2021. All right reserved.</p>
     </footer> -->
     <footer class="footer text-center text-lg-start py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 col-md-12 col-12">
-                    <div class="row">
-                        <div class="col-sm-6 col-lg-7 col-12 footer-nav-logo widget text-light mt-3">
-                            <a href="/" class="logo-wrapper" title="Evo Tools">
-                                <div class="d-flex justify-content-start">
-                                    <img src="https://cdn-icons-png.freepik.com/256/8523/8523899.png?ga=GA1.1.1642455953.1744362054&semt=ais_hybri"
-                                        style="height: 50px; width: 50px;" alt="Evo Tools" class="lazy loaded me-2"
-                                        data-was-processed="true">
-                                    <h4 class="mt-3"><b class="text-white">NFT CERTIFICATE</b></h4>
-                                </div>
+        <div class="row">
+            <div class="col-lg-8 col-md-12 ">
+                <div class="row">
+                    <div class="col-sm-8 col-lg-7  footer-nav-logo widget text-light mt-3 ">
+                        <a href="/" class="logo-wrapper" title="Evo Tools">
+                            <div class="d-flex justify-content-start ms-5">
+                                <img src="https://cdn-icons-png.freepik.com/256/8523/8523899.png?ga=GA1.1.1642455953.1744362054&semt=ais_hybri"
+                                    style="height: 50px; width: 50px;" alt="Evo Tools" class="lazy loaded me-2 ms-5"
+                                    data-was-processed="true">
+                                <h4 class="mt-3"><b class="text-white">NFT CERTIFICATE</b></h4>
+                            </div>
 
-                            </a>
-                            <label class="mt-2">Chứng nhận học vấn – Bảo mật tương lai bằng công nghệ NFT.</label>
+                        </a>
+                        <div class="ms-5">
+                            <label class="mt-2 ms-5">Chứng nhận học vấn – Bảo mật tương lai bằng công nghệ NFT.</label>
                             <br>
-                            <label class="mt-2">Bằng cấp số, niềm tin thực – NFT xác minh minh bạch.</label>
+                            <label class="mt-2 ms-5">Bằng cấp số, niềm tin thực – NFT xác minh minh bạch.</label>
 
-                            <p class="mt-2">An toàn, không thể làm giả – Chứng chỉ học vấn thời đại NFT.</p>
+                            <p class="mt-2 ms-5">An toàn, không thể làm giả – Chứng chỉ học vấn thời đại NFT.</p>
                         </div>
-                        <div class="col-sm-6 col-lg-5 col-12 footer-nav-info widget">
-                            <b>
-                                <h4 class="footer-title text-light mt-3">Về chúng tôi</h4>
-                            </b>
-                            <p class="text-light"><b><i class="fa-solid fa-phone"></i> SDT: </b>1900 6750</p>
+                    </div>
+                    <div class="col-sm-4 col-lg-5  footer-nav-info widget">
+                        <b>
+                            <h4 class="footer-title text-light mt-3">Về chúng tôi</h4>
+                        </b>
+                        <p class="text-light"><b><i class="fa-solid fa-phone"></i> SDT: </b>1900 6750</p>
 
-                            <p class="evo-in text-light"><strong><i class="fa-solid fa-location-dot"></i> Địa chỉ:
-                                </strong>242 Tô Hiệu, Thanh Khê Tây, Đà Nẵng
-                            </p>
+                        <p class="evo-in text-light"><strong><i class="fa-solid fa-location-dot"></i> Địa chỉ:
+                            </strong>242 Tô Hiệu, Thanh Khê Tây, Đà Nẵng
+                        </p>
 
-                            <p class="evo-in">
+                        <p class="evo-in">
 
-                                <strong class="text-light"><i class="fa-solid fa-square-envelope"></i> Email:
-                                </strong><a href="mailto:support@sapo.vn" style="color: white;" class="login-link"
-                                    title="support@sapo.vn">support@sapo.vn</a>
+                            <strong class="text-light"><i class="fa-solid fa-square-envelope"></i> Email:
+                            </strong><a href="mailto:support@sapo.vn" style="color: white;" class="login-link"
+                                title="support@sapo.vn">support@sapo.vn</a>
 
-                            </p>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12  fix-ipad-footer">
+                <div class="row">
+                    <div class="col-sm-6 col-lg-6 col-6 footer-nav-menu widget ">
+                        <b>
+                            <h4 class="footer-title text-light mt-3">Nếu bạn quan tâm</h4>
+                        </b>
+                        <div class="footer-menu">
+
+                            <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
+                                title="Trang chủ" rel="nofollow">Trang chủ</a>
+                            <br>
+                            <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
+                                title="Trang chủ" rel="nofollow">Giới thiệu</a>
+                            <br>
+                            <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
+                                title="Trang chủ" rel="nofollow">Liên hệ</a>
 
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-5 col-md-12 col-12 fix-ipad-footer">
-                    <div class="row">
-                        <div class="col-sm-6 col-lg-6 col-6 footer-nav-menu widget ">
-                            <b>
-                                <h4 class="footer-title text-light mt-3">Nếu bạn quan tâm</h4>
-                            </b>
-                            <div class="footer-menu">
-
-                                <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
-                                    title="Trang chủ" rel="nofollow">Trang chủ</a>
-                                <br>
-                                <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
-                                    title="Trang chủ" rel="nofollow">Giới thiệu</a>
-                                <br>
-                                <a href="/to-chuc-cap-chung-chi/quen-mat-khau" style="color: white;" class="login-link"
-                                    title="Trang chủ" rel="nofollow">Liên hệ</a>
-
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-lg-6 col-6 footer-nav-menu widget mt-3">
-                            <button class="btn btn-outline-light me-2"><i
-                                    class="fa-brands fa-facebook ms-1"></i></button>
-                            <button class="btn btn-outline-light me-2"><i
-                                    class="fa-brands fa-instagram ms-1"></i></button>
-                            <button class="btn btn-outline-light"><i class="fa-brands fa-telegram ms-1"></i></button>
-                        </div>
+                    <div class="col-sm-6 col-lg-6 col-6 footer-nav-menu widget mt-3">
+                        <button class="btn btn-outline-light me-2"><i class="fa-brands fa-facebook ms-1"></i></button>
+                        <button class="btn btn-outline-light me-2"><i class="fa-brands fa-instagram ms-1"></i></button>
+                        <button class="btn btn-outline-light"><i class="fa-brands fa-telegram ms-1"></i></button>
                     </div>
                 </div>
             </div>
