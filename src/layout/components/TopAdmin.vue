750d84fd5bcac6dff03aaeb50b0831f5583f507c
NPhLinh <nplinh22102003@gmail.com>
2025-04-17 20:43:06 +0700

    xd

--- src/layout/components/TopAdmin.vue
+++ src/layout/components/TopAdmin.vue
@@ -349,13 +349,9 @@
                         <li><a class="dropdown-item" href="/admin/thong-tin-admin"><i class="bx bx-user"></i><span>Thông
                                     Tin Tài Khoản</span></a>
                         </li>
-<<<<<<< HEAD
-                        <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-cog"></i><span>Đổi Mật
-                                    Khẩu</span></a>
-=======
                         <li><a class="dropdown-item" href="/admin/doi-mat-khau"><i
                                     class="bx bx-cog"></i><span>Đổi Mật Khẩu</span></a>
->>>>>>> a1ffa1b5f21b07ea3b10bcf31a42ea8810d9b83f
+
                         </li>
                         <li><a class="dropdown-item" href="javascript:;"><i
                                     class='bx bx-home-circle'></i><span>Dashboard</span></a>
