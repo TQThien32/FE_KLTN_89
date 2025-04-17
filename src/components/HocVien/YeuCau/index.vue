348897cb4527df494cdf91451c4bedc909943ab4
TQThien32 <tranqthien3203@gmail.com>
2025-04-18 01:00:55 +0700

    update thanh toan vs yeu cau

--- src/components/HocVien/YeuCau/index.vue
+++ src/components/HocVien/YeuCau/index.vue
@@ -1,42 +1,111 @@
 <template>
-    <div class="d-flex justify-content-between">
-            <h4 class="text-white"><i class="fa-solid fa-code-pull-request"></i> Yêu Cầu Cấp NFT</h4>
-            <button class="btn btn-outline-light me-5"> <i class="fa-solid fa-plus"></i> Tạo yêu cầu</button>
-        </div>
-        <div class="row mt-3">
-            <div class="col-lg-4">
-                <div class="card ">
-                    <div class="card-body text-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/7976/7976823.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
+    <h4 class="text-white mt-5"><i class="fa-solid fa-crown"></i> Đặc Quyền Khi Sở Hữu NFT Chứng Chỉ</h4>
+    <div class="row mt-5">
+        <div class="col-lg-4">
+            <div class="card card-dark">
+                <div class="card-body text-center">
+                    <img src="https://cdn-icons-png.freepik.com/256/7976/7976823.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
                         style="height: 70px;" alt="">
-                        <h4 class="card-title mt-2  text-light">NFT Chứng Chỉ</h4>
-                        <h6 class="card-subtitle mb-2 text-body-secondary text-light">Chứng chỉ độc nhất – xác thực tức thì.</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="card ">
-                    <div class="card-body text-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/4713/4713258.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
-                        style="height: 70px;" alt="">
-                        <h4 class="card-title mt-2  text-light">Gửi Yêu Cầu</h4>
-                        <h6 class="card-subtitle mb-2 text-body-secondary text-light">Một lần gửi – mãi mãi xác thực.</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="card ">
-                    <div class="card-body text-center">
-                        <img src="https://cdn-icons-png.freepik.com/256/11442/11442748.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
-                        style="height: 70px;" alt="">
-                        <h4 class="card-title mt-2  text-light">Bảo Mật</h4>
-                        <h6 class="card-subtitle mb-2 text-body-secondary text-light">Dữ liệu vĩnh viễn - quyền riêng tư tối đa.</h6>
-                    </div>
+                    <h4 class="card-title mt-2  text-light">NFT Chứng Chỉ</h4>
+                    <h6 class="card-subtitle mb-2 text-body-secondary text-light">Chứng chỉ độc nhất – xác thực tức thì.
+                    </h6>
                 </div>
             </div>
         </div>
-    <div class="row">
-        
+        <div class="col-lg-4">
+            <div class="card card-dark ">
+                <div class="card-body text-center">
+                    <img src="https://cdn-icons-png.freepik.com/256/4713/4713258.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
+                        style="height: 70px;" alt="">
+                    <h4 class="card-title mt-2  text-light">Gửi Yêu Cầu</h4>
+                    <h6 class="card-subtitle mb-2 text-body-secondary text-light">Một lần gửi – mãi mãi xác thực.</h6>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card card-dark">
+                <div class="card-body text-center">
+                    <img src="https://cdn-icons-png.freepik.com/256/11442/11442748.png?uid=R196496169&ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
+                        style="height: 70px;" alt="">
+                    <h4 class="card-title mt-2  text-light">Bảo Mật</h4>
+                    <h6 class="card-subtitle mb-2 text-body-secondary text-light">Dữ liệu vĩnh viễn - quyền riêng tư tối
+                        đa.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h4 class="text-white mt-5"><i class="fa-solid fa-code-pull-request"></i> Yêu Cầu Cấp NFT</h4>
+    <div class="card border-top border-0 border-4 border-info mt-5">
+        <div class="card-body mt-2">
+            <div class="card-title d-flex align-items-center">
+                <div><i class='bx bx-certification me-1 font-22'></i>
+                </div>
+                <h5 class="mb-0 ">Thông Tin Chứng Chỉ</h5>
+            </div>
+            <form class="row g-3 mt-2">
+                <div class="col-md-6">
+                    <label for="inputLastName1" class="form-label">Họ và Tên</label>
+                    <div class="input-group"> <span class="input-group-text bg-transparent"><i
+                                class='bx bxs-user'></i></span>
+                        <input type="text" class="form-control border-start-0" id="inputLastName1"
+                            placeholder="#Nguyen Van A" />
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label for="inputLastName2" class="form-label">Số CCCD</label>
+                    <div class="input-group"> <span class="input-group-text bg-transparent"><i
+                                class='bx bx-barcode'></i></span>
+                        <input type="text" class="form-control border-start-0" id="inputLastName2"
+                            placeholder="#049203..." />
+                    </div>
+                </div>
+                <div class="col-6">
+                    <label for="inputPhoneNo" class="form-label">Số Hiệu Chứng Chỉ</label>
+                    <div class="input-group"> <span class="input-group-text bg-transparent"><i
+                                class='bx bxs-file-doc'></i></span>
+                        <input type="text" class="form-control border-start-0" id="inputPhoneNo"
+                            placeholder="#598424" />
+                    </div>
+                </div>
+                <div class="col-6">
+                    <label for="inputEmailAddress" class="form-label">Email</label>
+                    <div class="input-group"> <span class="input-group-text bg-transparent"><i
+                                class='bx bxs-message'></i></span>
+                        <input type="text" class="form-control border-start-0" id="inputEmailAddress"
+                            placeholder="#abc@gmail.com" />
+                    </div>
+                </div>
+                <hr>
+                <div class="card-title d-flex align-items-center">
+                    <div><i class="bx bxs-school me-1 font-22 "></i>
+                    </div>
+                    <h5 class="mb-0 ">Thông Tin Tổ Chức</h5>
+                </div>
+                <div class="col-6">
+                    <label for="inputEmailAddress" class="form-label">Gửi Đến</label>
+                    <div class="input-group">
+                        <span class="input-group-text bg-white text-dark ">
+                            <i class='bx bxs-building-house'></i>
+                        </span>
+                        <select class="form-select bg-white text-dark ">
+                            <option value="">Trường Đại Học Duy Tân</option>
+                            <option value="">Trung tâm bồi dưỡng nghiệp vụ Liên Chiểu</option>
+                            <option value="">Tổ chức Giáo dục IIG Đà Nẵng</option>
+                        </select>
+                    </div>
+                </div>
+
+
+            </form>
+            <div class="col-12 text-end">
+                <button class="btn btn-info me-5" data-bs-toggle="modal" data-bs-target="#gui">Gửi Yêu Cầu</button>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row mt-5">
+
         <div class="col-lg-12 col-md-12 mt-5">
             <h4 class="text-white"><i class="fa-solid fa-list"></i> Danh Sách Yêu Cầu Đã Gửi</h4>
             <table class="table ">
@@ -46,6 +115,7 @@
                         <th>Gửi Đến</th>
                         <th>Thời Gian</th>
                         <th>Nội Dung</th>
+                        <th>Trạng Thái</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -53,8 +123,10 @@
                         <th>1</th>
                         <td>Đại Học Duy Tân</td>
                         <td>11:12 30/4/2025</td>
-                        <td><span class="badge text-bg-light" data-bs-toggle="modal" data-bs-target="#xemct">Xem chi
+                        <td><span class="badge text-bg-light hover-zoom" data-bs-toggle="modal"
+                                data-bs-target="#xemct">Xem chi
                                 tiết</span></td>
+                        <td><span class="badge text-bg-warning ">Chờ thanh toán</span></td>
 
                     </tr>
                 </tbody>
@@ -63,62 +135,71 @@
     </div>
     <!-- modal xem chi tiết -->
     <div class="modal fade" id="xemct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Yêu cầu đã gửi</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Thông tin yêu cầu</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <div class="container  p-4 bg-light rounded shadow">
+                        <div class="row mb-3">
+                            <div class="col-md-4">
+                                <label class="form-label fw-bold">Họ và tên:</label>
+                                <div class="form-control bg-white">Nguyễn Văn A</div>
+                            </div>
+                            <div class="col-md-4">
+                                <label class="form-label fw-bold">CCCD:</label>
+                                <div class="form-control bg-white">123456789012</div>
+                            </div>
+                            <div class="col-md-4">
+                                <label class="form-label fw-bold">Email:</label>
+                                <div class="form-control bg-white">nguyenvana@example.com</div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="form-label fw-bold">Số hiệu chứng chỉ:</label>
+                                <div class="form-control bg-white">ABC-2025-001</div>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label fw-bold">Gửi đến:</label>
+                                <div class="form-control bg-white">Trường Đại Học Duy Tân</div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+
                 </div>
             </div>
         </div>
     </div>
     <!-- Modal  gửi-->
-    <div class="modal fade" id="guiYeuCau" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="gui" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Có Gửi Yêu Cầu</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
+
                 <div class="modal-body">
-                    <div class="card">
-                        <div class="card-header ">
-                            <h4 class="">Gửi Yêu Cầu Cấp Chứng Chỉ</h4>
+                    <div class="alert alert-danger border-0 bg-info alert-dismissible fade show py-2 mt-2">
+                        <div class="d-flex align-items-center">
+                            <div class="font-35 text-white"><i class='bx bxs-message-square-x'></i>
+                            </div>
+                            <div class="ms-3">
+                                <h6 class="mb-0 text-white">Xác nhận gửi?</h6>
+                                <div class="text-white">Yêu cầu không thể khôi phục - Vui lòng kiểm tra kĩ trước khi gửi
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <label class="mt-2" for="">Tên Học Viên</label>
-                            <input class="mt-2 form-control" type="text">
-                            <label class="mt-2" for="">Số CCCD</label>
-                            <input class="mt-2 form-control" type="text">
-                            <label class="mt-2" for="">Số Hiệu Chứng Chỉ</label>
-                            <input class="mt-2 form-control" type="text">
-                            <label class="mt-2" for="">Email</label>
-                            <input class="mt-2 form-control" type="email">
-                            <label class="mt-2 w-100" for="">Gửi đến</label>
-                            <select class="mt-2 w-100" name="" id="" style="height: 38px;">
-                                <option value="">Tất cả</option>
-                                <option value="">Trường ....</option>
-                                <option value="">Tổ chức Toiec</option>
-                            </select>
-                            <label class="mt-2" for="">Nội Dung</label>
-                            <textarea class="mt-2 form-control" name="" id=""
-                                placeholder="Nhập nội dung yêu cầu"></textarea>
-                        </div>
-                        <div class="card-footer text-end">
-                            <button class="btn btn-chinh" data-bs-toggle="modal" data-bs-target="#XNgui">Gửi</button>
-                        </div>
+
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Gửi</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                    <button type="button" class="btn btn-info">Gửi</button>
                 </div>
             </div>
         </div>
@@ -133,15 +214,27 @@ export default {
 table tbody tr:hover {
     color: #d0cbca !important;
 }
-.card {
+
+.card-dark {
     border: 1px solid gray;
     border-radius: 0.5rem;
-    background-color: #111!important; /* hoặc bg-dark */
+    background-color: #111 !important;
+    /* hoặc bg-dark */
     transition: border-color 0.3s ease;
-    box-shadow: none; /* bỏ viền mờ */
+    box-shadow: none;
+    /* bỏ viền mờ */
 }
 
 .card:hover {
     border-color: white;
+}
+
+.hover-zoom {
+    transition: transform 0.2s ease;
+    cursor: pointer;
+}
+
+.hover-zoom:hover {
+    transform: scale(1.1);
 }
 </style>
