39cbef3dd583a88d6b17865ae22279d410117cbf
NPhLinh <nplinh22102003@gmail.com>
2025-04-22 00:26:30 +0700

    upfile

--- src/components/ToChucCapChungChi/ToChucUploadChungChiHocVien/index.vue
+++ src/components/ToChucCapChungChi/ToChucUploadChungChiHocVien/index.vue
@@ -13,64 +13,142 @@
          <div class="card-body table-responsive">
             <div class="row">
                <div class="col-lg-5">
-                  <form class="ff_fileupload_hidden" action="" method="post" enctype="multipart/form-data"><input
-                        type="hidden" name="action" value="fileuploader"><input type="file" name="files" multiple=""
-                        accept=".jpg, .png, image/jpeg, image/png"></form>
+                  <form>
+                     <input type="file" multiple webkitdirectory @change="onFolderSelect" />
+                  </form>
                </div>
-               <div class="col-lg-6"></div>
-               <div class="col-lg-1">
-                  <button class="btn btn-chinh me-2">Tải Lên</button>
+               <div class="col-lg-5">
+                  <button @click="uploadFolderImage" class="btn btn-chinh me-2">
+                     <span v-if="loadingFolder">Đang tải...</span>
+                     <span v-else>Up Folder Images</span>
+                  </button>
+               </div>
+               <div class="col-lg-5">
+                  <form class="ff_fileupload_hidden" enctype="multipart/form-data">
+                     <input type="file" @change="handleFileSelection"
+                        accept=".xls, .xlsx, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+                  </form>
+               </div>
+               <div class="col-lg-5">
+                  <button class="btn btn-chinh me-2" @click="uploadFile" :disabled="!selectedFile">
+                     <span v-if="loadingFile">Đang tải...</span>
+                     <span v-else>Up File Excel</span>
+                  </button>
                </div>
             </div>
             <table class="table table-hover table-bordered mt-3">
                <thead>
                   <tr>
-                     <th class="align-middle text-center">STT</th>
-                     <th class="align-middle text-center">Họ và tên</th>
-                     <th class="align-middle text-center">Email</th>
-                     <th class="align-middle text-center">Số Điện Thoại</th>
-                     <th class="align-middle text-center">Số Căn Cước Công Dân</th>
-                     <th class="align-middle text-center">Địa Chỉ</th>
-                     <th class="align-middle text-center">Số Hiệu Chứng Chỉ</th>
-                     <th class="align-middle text-center">Khóa Học</th>
-                     <th class="align-middle text-center">Trình Độ</th>
-                     <th class="align-middle text-center">Ngày Cấp</th>
-                     <th class="align-middle text-center">Kết Quả</th>
-                     <th class="align-middle text-center">Hình Ảnh</th>
+                     <th>STT</th>
+                     <th>Họ và tên</th>
+                     <th>Email</th>
+                     <th>Số Điện Thoại</th>
+                     <th>Số CCCD</th>
+                     <th>Địa Chỉ</th>
+                     <th>Số Hiệu Chứng Chỉ</th>
+                     <th>Khóa Học</th>
+                     <th>Trình Độ</th>
+                     <th>Ngày Cấp</th>
+                     <th>Kết Quả</th>
+                     <th>Hình Ảnh</th>
                   </tr>
                </thead>
                <tbody>
-                  <tr>
-                     <th class="align-middle text-center">1</th>
-                     <td class="align-middle text-center">Nguyễn Văn A</td>
-                     <td class="align-middle text-center">nguyenvana@gmail.com</td>
-                     <td class="align-middle text-center">0123456789</td>
-                     <td class="align-middle text-center">12345678901</td>
-                     <td class="align-middle text-center">123 Dũng Sĩ Thanh Khê</td>
-                     <td class="align-middle text-center">Học Viên</td>
-                     <td class="align-middle text-center">Học Viên</td>
-                     <td class="align-middle text-center">Học Viên</td>
-                     <td class="align-middle text-center">Học Viên</td>
-                     <td class="align-middle text-center">Học Viên</td>
-                     <td class="align-middle text-center">
-                        <img
-                           src="https://cdn-icons-png.freepik.com/256/818/818100.png?ga=GA1.1.1642455953.1744362054&semt=ais_hybrid"
-                           alt="" style="width: 50px; height: 50px; border-radius: 50%;">
+                  <tr v-for="(item, index) in dataList" :key="item.id">
+                     <th>{{ index + 1 }}</th>
+                     <td>{{ item.ten_hoc_vien }}</td>
+                     <td>{{ item.email }}</td>
+                     <td>{{ item.sdt }}</td>
+                     <td>{{ item.so_cccd }}</td>
+                     <td>{{ item.dia_chi }}</td>
+                     <td>{{ item.so_hieu_chung_chi }}</td>
+                     <td>{{ item.khoa_hoc }}</td>
+                     <td>{{ item.trinh_do }}</td>
+                     <td>{{ item.ngay_cap }}</td>
+                     <td>{{ item.ket_qua }}</td>
+                     <td>
+                        <img :src="item.hinh_anh" alt="Hình ảnh" style="width: 50px; height: 50px; border-radius: 50%;">
                      </td>
                   </tr>
-
-
                </tbody>
-
             </table>
          </div>
       </div>
    </div>
-
 </template>
-<script>
-export default {
 
-}
+<script>
+import axios from 'axios';
+
+export default {
+   data() {
+      return {
+         selectedFile: null,
+         files: [],
+         loadingFile: false,
+         loadingFolder: false,
+         dataList: []
+      };
+   },
+   methods: {
+      handleFileSelection(event) {
+         this.selectedFile = event.target.files[0];
+      },
+      async uploadFile() {
+         if (!this.selectedFile) {
+            alert("Vui lòng chọn một tệp Excel!");
+            return;
+         }
+
+         this.loadingFile = true;
+         let formData = new FormData();
+         formData.append("file", this.selectedFile);
+
+         try {
+            await axios.post("http://localhost:8000/api/import-excel", formData);
+            alert("Tải lên thành công!");
+            this.fetchData();
+         } catch (error) {
+            console.error("Lỗi khi tải lên:", error);
+         } finally {
+            this.loadingFile = false;
+         }
+      },
+      onFolderSelect(event) {
+         this.files = Array.from(event.target.files);
+      },
+      async uploadFolderImage() {
+         if (!this.files.length) {
+            alert("Vui lòng chọn thư mục!");
+            return;
+         }
+
+         this.loadingFolder = true;
+         const formData = new FormData();
+         this.files.forEach(file => {
+            formData.append("images[]", file, file.webkitRelativePath);
+         });
+
+         try {
+            await axios.post("http://localhost:8000/api/upload-folder", formData);
+            alert("Tải thư mục thành công!");
+         } catch (error) {
+            console.error("Lỗi khi tải thư mục:", error);
+         } finally {
+            this.loadingFolder = false;
+         }
+      },
+      async fetchData() {
+         try {
+            let response = await axios.get("http://localhost:8000/api/get-data");
+            this.dataList = response.data;
+         } catch (error) {
+            console.error("Lỗi khi lấy dữ liệu:", error);
+         }
+      }
+   },
+   mounted() {
+      this.fetchData();
+   }
+};
 </script>
-<style></style>
